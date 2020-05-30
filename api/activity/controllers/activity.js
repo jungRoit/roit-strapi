0c5a5a4c25bb3df9fd6b44dcc010f98294d9207b
jungRoit <roit.github@gmail.com>
2020-05-30 13:31:04 +0545

    added bulk price update endpoint

--- api/activity/controllers/activity.js
+++ api/activity/controllers/activity.js
@@ -5,4 +5,11 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  updatePrice: async ctx => {
+    const {discount} = ctx.request.body;
+    const data = await strapi.services.activity.updatePrice(ctx,discount);
+
+    ctx.send(data);
+  }
+};
--- api/activity/services/activity.js
+++ api/activity/services/activity.js
@@ -5,4 +5,21 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  updatePrice: async (ctx, discount) => {
+    const activities = await strapi.services.activity.find();
+
+    const updatedActivities = Promise.all(
+      activities.map(item => {
+        const data = {
+          ...item,
+          price: item.price - (item.price * (discount/100))
+        }
+
+        return strapi.services.activity.update({id:item.id},data)
+      })
+    );
+
+    return updatedActivities;
+  }
+};
